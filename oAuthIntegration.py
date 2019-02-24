79538708d66ad9410126f93f0b18ac6761f910e2
Rp300 <rohanp3425@gmail.com>
2019-02-24 00:57:51 -0600

    update

--- oAuthIntegration.py
+++ oAuthIntegration.py
@@ -1,6 +1,6 @@
 import webbrowser
 import requests
-redirect_uri=""
+redirect_uri="https://checkbook-facebookbot.herokuapp.com/redirect"
 client_id ="5633b82026504602837d70cf0a84323a"
 data = {
     'client_id':client_id,
@@ -8,5 +8,7 @@ data = {
     'scope': "check",
     'redirect_uri':redirect_uri
 }
-url = "https://checkbook.io/oauth/authorize?client_id=5633b82026504602837d70cf0a84323a&response_type=code&scope=check&redirect_uri=https://checkbook-facebookbot.herokuapp.com/"
-webbrowser.open(url)
+url = "https://sandbox.checkbook.io/oauth/authorize"
+r = requests.post(url=url, data = data)
+print(r.text)
+print(r.url)
